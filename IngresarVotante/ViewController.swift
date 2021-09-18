ffd74f9fbf1769bef7b904c2bfdac460bd05fdcf
Angelo Contreras <angelocontreras628@gmail.com>
2021-09-18 15:52:36 -0500

    Initial Commit

---
+++ IngresarVotante/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IngresarVotante
+//
+//  Created by Angelo Contreras on 18/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
